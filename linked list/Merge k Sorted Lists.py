8ad5d912525cfec4f17c102b3da855ac43a1d3e9
TanveenBal <tanveenbal@gmail.com>
2025-02-19 00:56:43 -0800

    Half of merge sort

---
+++ linked list/Merge k Sorted Lists.py
@@ -0,0 +1,39 @@
+from pydantic import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def merge(left: Optional[ListNode], right: Optional[ListNode]) -> Optional[ListNode]:
+    merged = ListNode()
+    curr = merged
+    while left and right:
+        if left.val < right.val:
+            curr.next = left
+            left = left.next
+        else:
+            curr.next = right
+            right = right.next
+        curr = curr.next
+    
+    if left:
+        curr.next = left
+    if right:
+        curr.next = right
+    
+    return merged.next
+
+
+def mergeKLists(lists: list[Optional[ListNode]]) -> Optional[ListNode]:
+    if not lists:
+        return None
+    if len(lists) == 1:
+        return lists[0]
+    
+    mid = len(lists) // 2
+    left = mergeKLists(lists[:mid])
+    right = mergeKLists(lists[mid:])
+    
+    return merge(left, right)
