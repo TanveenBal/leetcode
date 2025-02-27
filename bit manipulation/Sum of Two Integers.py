87dcdec0cae51c3f08504009f00ecdf9e25dd322
TanveenBal <tanveenbal@gmail.com>
2025-02-27 00:19:57 -0800

    Optimal bit addition

---
+++ bit manipulation/Sum of Two Integers.py
@@ -0,0 +1,14 @@
+def getSum(a: int, b: int) -> int:
+    mask = 0xFFFFFFFF
+    max_int = 0x7FFFFFFF
+
+    while b != 0:
+        carry = (a & b) << 1
+        a = (a ^ b) & mask
+        b = carry & mask
+
+    return a if a <= max_int else ~(a ^ mask)
+
+
+
+print(getSum(3, 2))
