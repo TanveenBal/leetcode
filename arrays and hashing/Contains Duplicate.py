5e5719e78b5cc45efa4803bddd2fe6ef0677080c
TanveenBal <tanveenbal@gmail.com>
2025-03-10 22:44:39 -0700

    contains duplicates for python

---
+++ arrays and hashing/Contains Duplicate.py
@@ -0,0 +1,9 @@
+def containsDuplicate(nums: list[int]) -> bool:
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        else:
+            seen.add(num)
+
+    return False
