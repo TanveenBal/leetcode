4a7bb6654e93a6844baddf59db094cd1bc66b111
TanveenBal <tanveenbal@gmail.com>
2025-03-11 02:11:27 -0700

    two sum

---
+++ arrays and hashing/Two Sum.py
@@ -0,0 +1,11 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    diffs = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+        if num in diffs:
+            return [diffs[num], i]
+        else:
+            diffs[diff] = i
+
+print(twoSum([3,4,5,6], 7))
