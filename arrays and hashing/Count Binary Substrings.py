1f785b323750a2610076eb9414c00e250cf204ad
TanveenBal <tanveenbal@gmail.com>
2025-03-13 14:59:57 -0700

    most optimal method

--- arrays and hashing/Count Binary Substrings.py
+++ arrays and hashing/Count Binary Substrings.py
@@ -1,19 +1,52 @@
 def countBinarySubstrings(s: str) -> int:
-    groups = [1]
     ans = 0
+    prev_run_length = 0  # Length of previous run (e.g., '00')
+    cur_run_length = 1   # Length of current run (start with 1 for first char)
 
-    length = len(s)
-    for i in range(1, length):
-        if s[i - 1] != s[i]:
-            groups.append(0)
-        groups[-1] += 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            cur_run_length += 1
+        else:
+            ans += min(prev_run_length, cur_run_length)
+            prev_run_length = cur_run_length
+            cur_run_length = 1  # Reset for new character run
 
-    length = len(groups)
-    for i in range(1, length):
-        ans += min(groups[i - 1], groups[i])
+    ans += min(prev_run_length, cur_run_length)  # Handle the last run
 
     return ans
 
+# def countBinarySubstrings(s: str) -> int:
+#     groups = [1]
+#     ans = 0
+#
+#     length = len(s)
+#     for i in range(1, length):
+#         if s[i - 1] != s[i]:
+#             groups.append(0)
+#         groups[-1] += 1
+#
+#     length = len(groups)
+#     for i in range(1, length):
+#         ans += min(groups[i - 1], groups[i])
+#
+#     return ans
+
+# def countBinarySubstrings(s: str) -> int:
+#     ans = 0
+#
+#     length = len(s)
+#     for i in range(1, length):
+#         l, r = i - 1, i
+#         l_char = s[l]
+#         r_char = s[r]
+#         if l_char == r_char:
+#             continue
+#         while l >= 0 and r < length and s[l] == l_char and s[r] == r_char:
+#             ans += 1
+#             l -= 1
+#             r += 1
+#
+#     return ans
 
 print(countBinarySubstrings("00110011"))
 print(countBinarySubstrings("10101"))
