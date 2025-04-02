ca2f771bc3191f91ce6ea6baf50bbc46076b1596
TanveenBal <tanveenbal@gmail.com>
2025-04-02 01:09:34 -0700

    bfs in c++ hard

---
+++ graphs/Number of Islands.cpp
@@ -0,0 +1,56 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  void bfs(int row, int col, vector<vector<char>> &grid,
+           vector<pair<int, int>> &dirs) {
+    int rows = grid.size();
+    int cols = grid[0].size();
+    queue<pair<int, int>> q;
+    q.push(make_pair(row, col));
+    grid[row][col] = '0'; // mark as visited
+
+    while (!q.empty()) {
+      pair<int, int> current = q.front();
+      q.pop();
+      int r = current.first;
+      int c = current.second;
+
+      for (size_t i = 0; i < dirs.size(); ++i) {
+        int dr = dirs[i].first;
+        int dc = dirs[i].second;
+        int nr = r + dr;
+        int nc = c + dc;
+
+        if (nr >= 0 && nr < rows && nc >= 0 && nc < cols &&
+            grid[nr][nc] == '1') {
+          q.push(make_pair(nr, nc));
+          grid[nr][nc] = '0'; // mark visited
+        }
+      }
+    }
+  }
+
+  int numIslands(vector<vector<char>> &grid) {
+    if (grid.empty())
+      return 0;
+
+    int count = 0;
+    vector<pair<int, int>> dirs{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    for (int r = 0; r < rows; ++r) {
+      for (int c = 0; c < cols; ++c) {
+        if (grid[r][c] == '1') {
+          ++count;
+          bfs(r, c, grid, dirs);
+        }
+      }
+    }
+    return count;
+  }
+};
