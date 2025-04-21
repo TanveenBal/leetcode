36f12a30ecf092b6b5c14ebbe4b371bc7cd6175d
TanveenBal <tanveenbal@gmail.com>
2025-04-21 16:54:02 -0700

    c# move zeroes

---
+++ arrays and hashing/Move Zeros.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int insert = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[insert] = nums[i];
+                insert++;
+            }
+        }
+
+        for (int i = insert; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+    }
+}
