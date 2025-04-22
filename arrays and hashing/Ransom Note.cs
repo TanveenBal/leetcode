af6b38e92bd05c3c187e6011973a35223237c2e9
TanveenBal <tanveenbal@gmail.com>
2025-04-21 21:44:59 -0700

    ransom note c# stuff

---
+++ arrays and hashing/Ransom Note.cs
@@ -0,0 +1,35 @@
+public class SolutionCanConstruct
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        Dictionary<char, int> freq = new Dictionary<char, int>();
+        foreach (char c in magazine)
+        {
+            if (freq.ContainsKey(c))
+            {
+                freq[c]++;
+            }
+            else
+            {
+                freq.Add(c, 1);
+            }
+        }
+
+        foreach (char c in ransomNote)
+        {
+            if (freq.ContainsKey(c))
+            {
+                freq[c]--;
+                if (freq[c] < 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
