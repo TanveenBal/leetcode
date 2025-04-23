91652bb8031b95ec715ab5e70b63c980d516d949
TanveenBal <tanveenbal@gmail.com>
2025-04-22 19:36:10 -0700

    just use set

---
+++ linked list/Intersection of Two Linked Lists.cs
@@ -0,0 +1,31 @@
+/*Definition for singly-linked list.*/
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class SolutionGetIntersectionNode
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        HashSet<ListNode> setA = new HashSet<ListNode>();
+
+        ListNode curr = headA;
+        while (curr != null)
+        {
+            setA.Add(curr);
+            curr = curr.next;
+        }
+
+        curr = headB;
+        while (curr != null)
+        {
+            if (setA.Contains(curr))
+                return curr;
+            curr = curr.next;
+        }
+        return null;
+    }
+}
