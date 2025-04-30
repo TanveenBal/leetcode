95db9104fae497b9cd9e663770e78a64ca164d3d
TanveenBal <tanveenbal@gmail.com>
2025-04-29 19:59:49 -0700

    without division

--- arrays and hashing/Product of Array Except Self.cs
+++ arrays and hashing/Product of Array Except Self.cs
@@ -2,40 +2,60 @@ public class SolutionProductExceptSelf
 {
     public int[] ProductExceptSelf(int[] nums)
     {
-        int zeroes = 0;
-        int zero_index = -1;
-        int totalProd = 1;
         int len = nums.Length;
+        int[] ans = new int[len];
+        int prefix = 1;
         for (int i = 0; i < len; i++)
         {
-            if (nums[i] == 0)
-            {
-                zeroes++;
-                if (zeroes > 1)
-                    break;
-                zero_index = i;
-            }
-            else
-                totalProd *= nums[i];
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
 
-        if (zeroes == 0)
+        int postfix = 1;
+        for (int i = len - 1; i >= 0; i--)
         {
-            for (int i = 0; i < len; i++)
-                nums[i] = totalProd / nums[i];
-        }
-        else if (zeroes == 1)
-        {
-            for (int i = 0; i < len; i++)
-                nums[i] = 0;
-            nums[zero_index] = totalProd;
-        }
-        else
-        {
-            for (int i = 0; i < len; i++)
-                nums[i] = 0;
+            ans[i] *= postfix;
+            postfix *= nums[i];
         }
 
-        return nums;
+        return ans;
     }
+    /*public int[] ProductExceptSelf(int[] nums)*/
+    /*{*/
+    /*    int zeroes = 0;*/
+    /*    int zero_index = -1;*/
+    /*    int totalProd = 1;*/
+    /*    int len = nums.Length;*/
+    /*    for (int i = 0; i < len; i++)*/
+    /*    {*/
+    /*        if (nums[i] == 0)*/
+    /*        {*/
+    /*            zeroes++;*/
+    /*            if (zeroes > 1)*/
+    /*                break;*/
+    /*            zero_index = i;*/
+    /*        }*/
+    /*        else*/
+    /*            totalProd *= nums[i];*/
+    /*    }*/
+    /**/
+    /*    if (zeroes == 0)*/
+    /*    {*/
+    /*        for (int i = 0; i < len; i++)*/
+    /*            nums[i] = totalProd / nums[i];*/
+    /*    }*/
+    /*    else if (zeroes == 1)*/
+    /*    {*/
+    /*        for (int i = 0; i < len; i++)*/
+    /*            nums[i] = 0;*/
+    /*        nums[zero_index] = totalProd;*/
+    /*    }*/
+    /*    else*/
+    /*    {*/
+    /*        for (int i = 0; i < len; i++)*/
+    /*            nums[i] = 0;*/
+    /*    }*/
+    /**/
+    /*    return nums;*/
+    /*}*/
 }
