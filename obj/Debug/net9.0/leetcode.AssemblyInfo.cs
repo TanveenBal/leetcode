c504da15b9c2259052caa4fde3bbf644c2917530
TanveenBal <tanveenbal@gmail.com>
2025-04-30 15:58:59 -0700

    sliding window, with o(1) check if we have chars

--- obj/Debug/net9.0/leetcode.AssemblyInfo.cs
+++ obj/Debug/net9.0/leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c5b3c3e01cc2ee608dd23a0c921f6ba57a998e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a74a8f702d502c6a0f20468a711d79ca166fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ sliding window/Minimum Window Substring.cs
@@ -0,0 +1,53 @@
+public class Solution
+{
+    public string MinWindow(string s, string t)
+    {
+        if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t)) return "";
+
+        Dictionary<char, int> need = new Dictionary<char, int>();
+
+        foreach (char c in t)
+        {
+            if (!need.ContainsKey(c)) need[c] = 0;
+            need[c]++;
+        }
+
+        Dictionary<char, int> have = new Dictionary<char, int>();
+        int l = 0, r = 0;
+        int valid = 0;
+        int start = 0, len = int.MaxValue;
+
+        while (r < s.Length)
+        {
+            char c = s[r];
+            r++;
+
+            if (need.ContainsKey(c))
+            {
+                if (!have.ContainsKey(c)) have[c] = 0;
+                have[c]++;
+                if (have[c] == need[c]) valid++;
+            }
+
+            while (valid == need.Count)
+            {
+                if (r - l < len)
+                {
+                    start = l;
+                    len = r - l;
+                }
+
+                char d = s[l];
+                l++;
+
+                if (need.ContainsKey(d))
+                {
+                    if (have[d] == need[d]) valid--;
+                    have[d]--;
+                }
+            }
+        }
+
+        return len == int.MaxValue ? "" : s.Substring(start, len);
+    }
+}
