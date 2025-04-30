6655b92230753a09c234f7c1574436b0f5407f42
TanveenBal <tanveenbal@gmail.com>
2025-04-30 14:21:35 -0700

    contain the location of start of window

--- obj/Debug/net9.0/leetcode.AssemblyInfo.cs
+++ obj/Debug/net9.0/leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5690807d47419115901aad07dfea98bd59ca5b60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8132ef6d6e9d93a9366fed0e6c8e8d3ffe9e4190")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ sliding window/Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        Dictionary<char, int> seen = new Dictionary<char, int>();
+        int start = 0, maxLength = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (seen.ContainsKey(s[i]) && seen[s[i]] >= start)
+            {
+                start = seen[s[i]] + 1;
+            }
+
+            seen[s[i]] = i;
+            maxLength = Math.Max(maxLength, i - start + 1);
+        }
+
+        return maxLength;
+    }
+}
