fdd3e7d8ab0154cf48dabbb81550798b64192ee3
TanveenBal <tanveenbal@gmail.com>
2025-05-04 21:41:10 -0700

    bin search recursive

---
+++ binary search/Binary Search.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    private int BinarySearch(int[] nums, int target, int l, int h)
+    {
+        if (l > h)
+            return -1;
+        int m = l + ((h - l) / 2);
+
+        if (target > nums[m])
+            return BinarySearch(nums, target, m + 1, h);
+        else if (target < nums[m])
+            return BinarySearch(nums, target, l, m - 1);
+        else
+            return m;
+    }
+    public int Search(int[] nums, int target)
+    {
+        return BinarySearch(nums, target, 0, nums.Length - 1);
+    }
+}
