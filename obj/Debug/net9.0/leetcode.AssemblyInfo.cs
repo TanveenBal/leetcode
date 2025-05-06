1ffcdeccb7cb58a8be20dd0263488b95927529f4
TanveenBal <tanveenbal@gmail.com>
2025-05-05 23:55:03 -0700

    renamed for lsp

--- obj/Debug/net9.0/leetcode.AssemblyInfo.cs
+++ obj/Debug/net9.0/leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d806845dd4ffdc0e0d20210e45bd98bbedb621f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acd76d79d1fece3db01751d8a7d31fa2af70306")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- trees/Construct Binary Tree from Preorder and Inorder Traversal.cs
+++ trees/Construct Binary Tree from Preorder and Inorder Traversal.cs
@@ -11,7 +11,7 @@
  *     }
  * }
  */
-public class Solution
+public class SolutionBuildTree
 {
     public class TreeNode
     {
