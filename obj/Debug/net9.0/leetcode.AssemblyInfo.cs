25475fc1ec5028b97f751fc2eab299e95cbbf90b
TanveenBal <tanveenbal@gmail.com>
2025-05-21 16:17:04 -0700

    stack calc time to push onto stack if car that is further behind is faster than car ahead then they become fleet

--- obj/Debug/net9.0/leetcode.AssemblyInfo.cs
+++ obj/Debug/net9.0/leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564082074a03d2a641d5cb6eb7e4ba22cc89bba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cd3c04fb338bc405c8e3d98a5d34c118b5901a")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- stack/Car Fleet.cs
+++ stack/Car Fleet.cs
@@ -2,6 +2,24 @@ public class SolutionCarFleet
 {
     public int CarFleet(int target, int[] position, int[] speed)
     {
-        return 0;
+        int n = position.Length;
+        if (n == 0) return 0;
+
+        (int pos, int spd)[] p_s = new (int, int)[n];
+        for (int i = 0; i < n; ++i)
+            p_s[i] = (position[i], speed[i]);
+
+        Array.Sort(p_s, (a, b) => b.pos.CompareTo(a.pos));
+
+        Stack<double> times = new Stack<double>();
+
+        for (int i = 0; i < n; ++i)
+        {
+            double time = (double)(target - p_s[i].pos) / p_s[i].spd;
+            if (times.Count == 0 || time > times.Peek())
+                times.Push(time);
+        }
+
+        return times.Count;
     }
 }
