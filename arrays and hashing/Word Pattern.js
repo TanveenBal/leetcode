c3defb0300a0c39d924a57b926896e135eba15f0
TanveenBal <tanveenbal@gmail.com>
2025-06-21 01:48:46 -0700

    looks nicer

--- arrays and hashing/Word Pattern.js
+++ arrays and hashing/Word Pattern.js
@@ -7,17 +7,17 @@ var wordPattern = function (pattern, s) {
   const strings = s.split(" ");
   if (strings.length !== pattern.length) return false;
 
-  const mapps = new Map();
-  const mapsp = new Map();
+  const ps = new Map();
+  const sp = new Map();
 
   for (let i = 0; i < pattern.length; ++i) {
     const p = pattern[i];
     const word = strings[i];
 
-    if (!mapps.has(p)) mapps.set(p, word);
-    if (!mapsp.has(word)) mapsp.set(word, p);
+    if (!ps.has(p)) ps.set(p, word);
+    if (!sp.has(word)) sp.set(word, p);
 
-    if (mapps.get(p) !== word || mapsp.get(word) !== p) return false;
+    if (ps.get(p) !== word || sp.get(word) !== p) return false;
   }
 
   return true;
